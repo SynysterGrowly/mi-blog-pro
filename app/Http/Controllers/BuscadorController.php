7e6e7513003d81193bbb0b3365d8d83dad036f83
Suemy Ag <sistemas@pms.edu.mx>
2025-03-14 12:44:33 -0500

    [ADD] Uso de like para aprovechar la búsqueda y tener más resultados

--- app/Http/Controllers/BuscadorController.php
+++ app/Http/Controllers/BuscadorController.php
@@ -8,13 +8,16 @@ use App\Models\Category;
 
 
 class BuscadorController extends Controller
+
 {
     public function buscar(Request $request)
     {
         $query = $request->input('query');
 
-        $categorias = Category::where('nombre', $query)->get();
-        $entradas = Entrada::where('titulo', $query)->get();
+      //Manera de traer las palabra clave sin tener que ser identico, se encontrarán más resultados
+
+        $categorias = Category::where('nombre', 'LIKE', "%{$query}%")->get();
+        $entradas = Entrada::where('titulo', 'LIKE', "%{$query}%")->get();
 
         return view('webpage.resultados', compact('entradas', 'categorias', 'query'));
     }
